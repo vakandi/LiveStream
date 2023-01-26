1a8d31aba87a5c8ab3e0b1ea6c60cc8ac7b984ed
vakandi <wael.bousfira@protonmail.com>
2023-01-26 03:29:13 +0100

    update

---
+++ server/install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install OpenCV
+brew install opencv
+
+# Install PyAudio
+brew install portaudio
+pip install pyaudio
+
+# Install Flask
+pip install flask
+
+# Install PHP
+brew install php
+
+
